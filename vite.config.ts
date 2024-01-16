f6aaa827b63f62d4f6c75dcc6450d67fa2c2cb1f
rubtsovi <rubtsovi.90@gmail.com>
2024-01-16 13:19:16 +0100

    environment configured (I hope...)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,45 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import viteBasicSslPlugin from '@vitejs/plugin-basic-ssl';
+import vue from '@vitejs/plugin-vue';
+import dns from 'dns';
+import fs from 'fs';
+import path from 'path';
+import { CommonServerOptions, defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+const IS_SECURE = process.env.IS_SECURE === 'true';
+
+const vitePlugins = [vue(), tsconfigPaths()];
+let httpsConfig: CommonServerOptions['https'];
+if (IS_SECURE) {
+  configureSsl();
+  dns.setDefaultResultOrder('verbatim');
+}
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default defineConfig(({ mode }) => ({
+  plugins: vitePlugins,
+  server: {
+    https: httpsConfig,
+    host: IS_SECURE ? 'localhost' : '127.0.0.1',
+  },
+  build: {
+    minify: mode === 'production' ? 'esbuild' : false,
+    sourcemap: mode === 'staging',
+  },
+}));
+
+function configureSsl() {
+  const certs = path.join(__dirname, '.cert'),
+    sslCertPath = path.join(certs, 'localhost.pem'),
+    sslKeyPath = path.join(certs, 'localhost-key.pem');
+
+  if (fs.existsSync(sslCertPath) && fs.existsSync(sslKeyPath)) {
+    httpsConfig = {
+      key: sslKeyPath,
+      cert: sslCertPath,
+    };
+    return;
+  }
+
+  vitePlugins.push(viteBasicSslPlugin());
+}
